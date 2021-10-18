26702d023ef4467bbdf8ca80b713150294427989
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-10-18 16:10:35 -0500

    installed packages and created package-lock.json and package.json

---
+++ index.js
@@ -0,0 +1,6 @@
+// Need to use MySQL2 package
+const mysql = require("mysql2");
+// Need inquirer
+const inquirer = require("inquirer");
+// console.table package needed to print MySQL rows to console
+const consTable = require("console.table");
---
+++ schema.sql
@@ -0,0 +1,18 @@
+-- TO DO: create the following three tables
+-- Department table
+    -- id: INT PRIMARY KEY
+    -- name: VARCHAR (30)
+-- Role
+    -- id: INT PRIMARY KEY
+    -- title: VARCHAR (30)
+    -- salary: DECIMAL
+    -- department_id: INT
+-- Employee
+    -- id: INT PRIMARY KEY
+    -- first_name: VARCHAR (30)
+    -- last_name: VARCHAR (30)
+    -- role_id: INT
+    -- manager_id: INT (null if the employee has no manager)
+
+-- Might want to include a seeds.sql
+-- Might want to use seperate file that contains functions for performing specific SQL queries you'll need to use.
