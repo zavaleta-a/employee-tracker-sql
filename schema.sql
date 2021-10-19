55134aa7b89259d00e1a236c1d9e3c16fa2072e6
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-10-19 17:59:35 -0500

    created tables code for department, role, and employee

--- schema.sql
+++ schema.sql
@@ -14,12 +14,23 @@ CREATE TABLE department (
     -- title: VARCHAR (30)
     -- salary: DECIMAL
     -- department_id: INT
+CREATE TABLE role (
+    id: INT NOT NULL PRIMARY KEY,
+    title: VARCHAR (30),
+    salary: DECIMAL,
+    department_id: INT
+);
 -- Employee
     -- id: INT PRIMARY KEY
     -- first_name: VARCHAR (30)
     -- last_name: VARCHAR (30)
     -- role_id: INT
     -- manager_id: INT (null if the employee has no manager)
-
+CREATE TABLE employee (
+    id: INT PRIMARY KEY,
+    first_name: VARCHAR (30),
+    last_name: VARCHAR (30),
+    role_id: INT
+)
 -- Might want to include a seeds.sql
 -- Might want to use seperate file that contains functions for performing specific SQL queries you'll need to use.
