755dc530b59118fb7199161332ce6f454da7b149
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-10-19 18:06:45 -0500

    created seeds.sql and created seeds for department, employee, and employee role

---
+++ seeds.sql
@@ -0,0 +1,23 @@
+-- Department Seeds
+INSERT INTO department (name)
+VALUE ('Sales');
+INSERT INTO department (name)
+VALUE ('Engineering');
+INSERT INTO department (name)
+VALUE ('Accounting');
+INSERT INTO department (name)
+VALUE ('Rooms');
+
+-- Employee Seeds
+INSERT INTO employee (last_name, first_name, manager_id, role_id)
+VALUE ('');
+
+-- Employee Role Seeds
+INSERT INTO role (title, salary, department_id)
+VALUE ('');
+INSERT INTO role (title, salary, department_id)
+VALUE ('');
+INSERT INTO role (title, salary, department_id)
+VALUE ('');
+INSERT INTO role (title, salary, department_id)
+VALUE ('');
